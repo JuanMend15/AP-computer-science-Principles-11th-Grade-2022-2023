26f813e9efa619de90a5d9ddcc1430ce02fa5585
JuanMend15 <111314211+JuanMend15@users.noreply.github.com>
2022-08-22 15:00:12 -0600

    Update phytonVariables.py

--- phytonVariables.py
+++ phytonVariables.py
@@ -1,1 +1,44 @@
+x=5
+y="John"
+print(x)
+print(y)
+#5=integer=int
+#hello=word=integer\
+#Boolean @ and 1
+#3.0=float#Variables are case sensitives 
+a=4
+A= "Bob"
+print(a,A)
+#A will not overwrite a 
+# =in progamming means "assigned to" example 5 is assigned to variable x
+# LEGAL VARIBLE NAMES
+#  A variable name must start or underscore character
+# It cannot start with a number 
+#IS A CASE SENSITIVE ADN CANNOT contain Alpha-Numeric characters (A-z,0-9)
+# the "#" can also mean  "man equal to"
+# phython allow you to assign many values to multiple variables 
+x,y,z= "Orange","Banana", "cherry"
+print(x)
+print(y)
+print(z)
+# phython allows you to assing multiple variables 
+x=y=z="Orange"
+print(x)
+print(y)
+print(z)
 
+
+
+#Random Number 
+import random 
+print(random.randrange(1,10))  
+# concatination 
+# to concatinate or combine 2 strings you can use the + operator
+a= "Hello"
+b="patricia"
+c= a + "" + b
+print(c)
+# String format 
+age ="36"
+text="My name is Patricia ,Iam "+ age 
+print( text )
